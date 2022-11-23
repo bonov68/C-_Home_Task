b46e1bce0ef95e8f630a97a59a0fd7ad93ff6bd9
Antonina <bonov68@mail.ru>
2022-11-24 00:35:46 +0500

    вариант сравнения не верен, продумать другой!

--- h_task2/Program.cs
+++ h_task2/Program.cs
@@ -2,3 +2,5 @@
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число__");
 int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int comp = Convert.ToInt32(Console.WriteLine(num1 > num2));
