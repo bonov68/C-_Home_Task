ba7d09c16052a524ba91485d48581f60e4d36127
Antonina <bonov68@mail.ru>
2022-11-24 13:29:58 +0500

    added 3 input string

---
+++ h_task_4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число_");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число__");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число_");
+int num3 = Convert.ToInt32(Console.ReadLine());
