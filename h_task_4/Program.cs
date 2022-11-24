9b00cd074257f6734c647822a2c563f05a577f1b
Antonina <bonov68@mail.ru>
2022-11-24 14:16:43 +0500

    add else, task done

--- h_task_4/Program.cs
+++ h_task_4/Program.cs
@@ -5,6 +5,12 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число_");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2 & num1 > num3) {
+if (num1 >= num2 & num1 >= num3) {
     Console.WriteLine ($"{num1} максимальное число");
+}
+else if (num2 >= num1 & num2 >= num3) {
+        Console.WriteLine ($"{num2} максимальное число");
+}
+else if (num3 >= num1 & num3 >= num2) {
+        Console.WriteLine ($"{num3} максимальное число");
 }
