b0986e40c60493b45c175fa7b0826df705ace69c
Antonina <bonov68@mail.ru>
2022-11-24 12:49:33 +0500

    correct interpolation, task done

--- h_task2/Program.cs
+++ h_task2/Program.cs
@@ -4,10 +4,9 @@ Console.WriteLine("Введите число__");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2) {
-    
-    Console.WriteLine($"num1 больше num2");
+
+    Console.WriteLine($"{num1} больше {num2}");
 }
 else {
-    Console.WriteLine($"num1 меньше num2");
+    Console.WriteLine($"{num1} меньше {num2}");
 }
-// int comp = Convert.ToInt32(Console.WriteLine(num1 > num2));
