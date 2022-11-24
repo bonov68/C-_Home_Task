587e4b20408885e12c6797b6bf3c2902fe3e0761
Antonina <bonov68@mail.ru>
2022-11-24 13:46:46 +0500

    add 1 if, result correct, need add 2 if

--- h_task_4/Program.cs
+++ h_task_4/Program.cs
@@ -4,3 +4,7 @@ Console.WriteLine("Введите число__");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число_");
 int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 & num1 > num3) {
+    Console.WriteLine ($"{num1} максимальное число");
+}
