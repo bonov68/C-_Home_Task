6777263177a93c9df35302949028e3c9c67c8880
Antonina <bonov68@mail.ru>
2022-11-24 14:43:24 +0500

    task done

---
+++ h_task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число_");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) {
+    Console.WriteLine("это четное число");
+}
+else {
+    Console.WriteLine("это нечетное число");
+}
